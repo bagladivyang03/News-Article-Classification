ed6a1e3a6677ca1c7b013e0b6d864f143e659498
Divyang Bagla <bagladivyang03@gmail.com>
2021-06-17 23:04:32 +0530

    basic layout done to display all artciles

--- app.py
+++ app.py
@@ -20,6 +20,19 @@ def home():
       "apiKey": API_KEY
     }
     main_url = "https://newsapi.org/v2/top-headlines"
+    # url = "https://bing-news-search1.p.rapidapi.com/news"
+
+    # querystring = {"textFormat":"Raw","safeSearch":"Off"}
+
+    # headers = {
+    #     'x-bingapis-sdk': "true",
+    #     'x-rapidapi-key': "a44c138188msh26db5a879ce4a96p1198c1jsn239b6a0ac379",
+    #     'x-rapidapi-host': "bing-news-search1.p.rapidapi.com"
+    #     }
+
+    # response = requests.request("GET", url, headers=headers, params=querystring)
+    # print(len(response.json()['value']))
+    # print(response.text.json())
     res = requests.get(main_url, params=query_params)
     articles_requested = res.json()
     print(articles_requested)
@@ -33,7 +46,7 @@ def home():
 
 @app.route('/newsarticle/<varible_name>/')
 def article(varible_name):
-    return render_template('result.html',title=varible_name)
+    return render_template('article.html',title=varible_name)
 
 if __name__ == '__main__':
 	app.run(debug=True)
